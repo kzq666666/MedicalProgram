fa19b18cb1b61af4bdac114d28cc2077d9d849eb
kzq666666 <kzqkangzhiqing@gmail.com>
2021-10-13 22:18:56 +0800

    update

---
+++ components/logoPage/logoPage.js
@@ -0,0 +1,23 @@
+// components/logoPage/logoPage.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    title: "医疗系统"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
